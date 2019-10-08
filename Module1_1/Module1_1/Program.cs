eafa7783fa9da1afaa94b3e92cec389aa38753bd
tohajy <tohajy@gmail.com>
2019-10-08 23:17:44 +0300

    Task 1 was completed.

--- Module1_1/Module1_1/Program.cs
+++ Module1_1/Module1_1/Program.cs
@@ -10,6 +10,12 @@ namespace Module1_1
     {
         static void Main(string[] args)
         {
+            int a = 1, b = 2, temp;
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("a: " + a);
+            Console.WriteLine("b: " + b);
         }
     }
 }
