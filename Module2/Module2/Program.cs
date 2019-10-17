aaa65e33ce639aad25eb0208b0abd2742f94719c
tohajy <tohajy@gmail.com>
2019-10-17 21:54:39 +0300

    module 2_1

---
+++ Module2/Module2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCompanies = 0;
+            double tax = 0;
+            double profit = 500;
+            double sumTaxes;
+            bool condition = false;
+
+            while (!condition)
+            {
+                Console.WriteLine("Введите число компаний:"); condition = int.TryParse(Console.ReadLine(), out numberCompanies);
+            }
+
+            condition = false;
+            while (!condition)
+            {
+                Console.WriteLine("Введите налог в %:"); condition = double.TryParse(Console.ReadLine(), out tax);
+            }
+
+            sumTaxes = numberCompanies * profit * tax / 100;
+
+            Console.WriteLine($"Сумма общих налогов: {sumTaxes} рублей");
+            Console.ReadKey();
+        }
+    }
+}
