399f50bd675f858697bac2a56076cf17b59cc547
tohajy <tohajy@gmail.com>
2019-10-17 22:53:38 +0300

    Task 4 was completed

--- Module2/Module2_4/Program.cs
+++ Module2/Module2_4/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Module2_4
 {
@@ -12,6 +8,7 @@ namespace Module2_4
         {
             Console.WriteLine("Выберите фигуру для расчета. 3 - треугольник, 4 - прямоугольник, 0 - круг.");
             string selection = Console.ReadLine();
+
             switch (selection)
             {
                 case "3":
@@ -20,36 +17,58 @@ namespace Module2_4
                     Console.WriteLine("Введите катет 2:");
                     Formulas.sCathetus = Convert.ToDouble(Console.ReadLine());
                     Console.WriteLine("Что будем считать? s - площадь, p - периметр.");
-                    if (Console.ReadLine() == "s")
+
+                    string taskTriangle = Console.ReadLine();
+
+                    if (taskTriangle == "s")
                     {
                         Formulas.SquareTriangle(Formulas.fCathetus, Formulas.sCathetus);
                     }
-                    else if (Console.ReadLine() == "p")
+                    else if (taskTriangle == "p")
                     {
                         Formulas.PerimeterTriangle(Formulas.fCathetus, Formulas.sCathetus);
                     }
+
                     break;
+
                 case "4":
                     Console.Write("Введите первую сторону прямоугольника: ");
                     Formulas.a = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Введите вторую сторону прямоугольника: ");
                     Formulas.b = Convert.ToDouble(Console.ReadLine());
                     Console.WriteLine("Что будем считать? s - площадь, p - периметр.");
-                    if (Console.ReadLine() == "s")
+
+                    string taskRectangle = Console.ReadLine();
+
+                    if (taskRectangle == "s")
                     {
                         Formulas.SquareRectangle(Formulas.a, Formulas.b);
                     }
-                    else if (Console.ReadLine() == "p")
+                    else if (taskRectangle == "p")
                     {
                         Formulas.PerimeterRectangle(Formulas.a, Formulas.b);
                     }
+
                     break;
+
                 case "0":
                     Console.Write("Введите радиус круга: ");
                     Formulas.radius = Convert.ToDouble(Console.ReadLine());
                     Console.WriteLine("Что будем считать? s - площадь, p - периметр.");
 
+                    string taskCircle = Console.ReadLine();
+
+                    if (taskCircle == "s")
+                    {
+                        Formulas.SquareCircle(Formulas.radius);
+                    }
+                    else if (taskCircle == "p")
+                    {
+                        Formulas.PerimeterCircle(Formulas.radius);
+                    }
+
                     break;
+
                 default:
                     Console.WriteLine("Вы нажали неизвестную букву");
                     break;
@@ -70,56 +89,74 @@ namespace Module2_4
         public static void SquareTriangle(double firstcatet, double secondcatet)
         {
             double S = fCathetus * sCathetus / 2;
+
             Console.WriteLine("Площадь треугольника = {0}", S);
+
             double pSquare = 4 * Math.Sqrt(S);
             double pCircle = Math.Sqrt(4 * Math.PI * S);
+
             Console.WriteLine($"При заданной площади ({S}) периметр квадрата = {pSquare}");
             Console.WriteLine($"При заданной площади ({S}) периметр круга = {pCircle}");
         }
-        public static void PerimeterTriangle(double fcatet, double scatet)
+        public static void PerimeterTriangle(double firstcatet, double secondcatet)
         {
             double P = fCathetus + sCathetus + Math.Sqrt(fCathetus * fCathetus + sCathetus * sCathetus);
+
             Console.WriteLine("Периметр треугольника = {0}", P);
+
             double sSquare = P * P / 16;
             double sCircle = P * P / (4 * Math.PI);
+
             Console.WriteLine($"При заданном периметре ({P}) площадь квадрата = {sSquare}");
             Console.WriteLine($"При заданной площади ({P}) площадь круга = {sCircle}");
         }
         public static void SquareRectangle(double aside, double bside)
         {
             double S = a * b;
+
             Console.WriteLine("Площадь прямоугольника = {0}", S);
+
             double pTriangle = Math.Sqrt(2 * S)+ Math.Sqrt(2 * S) + Math.Sqrt(4 * S);
             double pCircle = Math.Sqrt(4 * Math.PI * S);
+
             Console.WriteLine($"При заданной площади ({S}) периметр треугольника = {pTriangle}");
             Console.WriteLine($"При заданной площади ({S}) периметр круга = {pCircle}");
         }
-        public static void PerimeterRectangle(double fcatet, double scatet)
+        public static void PerimeterRectangle(double aside, double bside)
         {
             double P = 2*(a+b);
+
             Console.WriteLine("Периметр прямоугольника = {0}", P);
-            double sTriangle = P / 18;
+
+            double sTriangle = P * P / 18;
             double sCircle = P * P / (4 * Math.PI);
+
             Console.WriteLine($"При заданном периметре ({P}) площадь равностороннего треугольника = {sTriangle}");
             Console.WriteLine($"При заданной площади ({P}) площадь круга = {sCircle}");
         }
         public static void SquareCircle(double rad)
         {
-            double S = a * b;
-            Console.WriteLine("Площадь прямоугольника = {0}", S);
+            double S = Math.PI * radius * radius;
+
+            Console.WriteLine("Площадь круга = {0}", S);
+
             double pTriangle = Math.Sqrt(2 * S) + Math.Sqrt(2 * S) + Math.Sqrt(4 * S);
-            double pCircle = Math.Sqrt(4 * Math.PI * S);
+            double pSquare = 4 * Math.Sqrt(S);
+
             Console.WriteLine($"При заданной площади ({S}) периметр треугольника = {pTriangle}");
-            Console.WriteLine($"При заданной площади ({S}) периметр круга = {pCircle}");
+            Console.WriteLine($"При заданной площади ({S}) периметр квадрата = {pSquare}");
         }
         public static void PerimeterCircle(double rad)
         {
-            double P = 2 * (a + b);
-            Console.WriteLine("Периметр прямоугольника = {0}", P);
-            double sTriangle = P / 18;
-            double sCircle = P * P / (4 * Math.PI);
+            double P = 2 * Math.PI * radius;
+
+            Console.WriteLine("Длина окружности = {0}", P);
+
+            double sTriangle = P * P / 18;
+            double sSquare = P * P / 16;
+
             Console.WriteLine($"При заданном периметре ({P}) площадь равностороннего треугольника = {sTriangle}");
-            Console.WriteLine($"При заданной площади ({P}) площадь круга = {sCircle}");
+            Console.WriteLine($"При заданной площади ({P}) площадь квадрата = {sSquare}");
         }
     }
 }
