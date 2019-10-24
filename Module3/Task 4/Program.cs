416ee85ce3ded5b310960f78880a1e0aa68e8b74
tohajy <tohajy@gmail.com>
2019-10-24 21:57:50 +0300

    Task 4

---
+++ Module3/Task 4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int newNumber = 0;
+            int number;
+            int temp;
+
+            Console.WriteLine("Введите число:");
+
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("Введённое вами значение некорректное. Попробуйте ещё раз: ");
+            }
+            while (number != 0)
+            {
+                temp = number % 10;
+                number = number / 10;
+                newNumber = newNumber * 10 + temp;
+            }
+            Console.WriteLine("Преобразованное число: " + newNumber);
+            Console.ReadLine();
+        }
+    }
+}
