0ebed55f59007e4df810a8d7b5078aa6d966091f
tohajy <tohajy@gmail.com>
2019-10-31 23:29:25 +0300

    Task 1

--- Module 4/Task 1/Program.cs
+++ Module 4/Task 1/Program.cs
@@ -1,15 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var arraySize = 0;
+
+            Console.WriteLine("Array size:");
+
+            while (!int.TryParse(Console.ReadLine(), out arraySize))
+            {
+                Console.Write("Incorrect value. Try again: ");
+            }
+
+            var rnd = new Random();
+            var array = new int[arraySize];
+
+            Console.WriteLine("Array:");
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(-100, 100);
+                Console.Write(array[i] + " ");
+            }
+
+            var max = GetMaxValue(array);
+            var min = GetMinValue(array);
+            var sumArr = GetSum(array);
+            var difArr = GetDifferenceMaxMin(array);
+
+            GetModifyArray(array);
+
+            Console.WriteLine();
+            Console.WriteLine($"Max value - ({max})");
+            Console.WriteLine($"Min value - ({min})");
+            Console.WriteLine($"Sum  - ({sumArr})");
+            Console.WriteLine($"Difference between Max and Min  - ({difArr})");
+            Console.WriteLine("Modified Array: ");
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int GetMaxValue(int[] arr)
+        {
+            var maxValue = arr[0];
+            maxValue = arr.Concat(new[] { maxValue }).Max();
+            return maxValue;
+        }
+
+        private static int GetMinValue(int[] arr)
+        {
+            var minValue = arr[0];
+            minValue = arr.Concat(new[] { minValue }).Min();
+            return minValue;
+        }
+
+        private static int GetSum(int[] arr)
+        {
+            var sum = arr.Sum();
+            return sum;
+        }
+
+        private static int GetDifferenceMaxMin(int[] arr)
+        {
+            var maxValue = arr[0];
+            var minValue = arr[0];
+
+            maxValue = arr.Concat(new[] { maxValue }).Max();
+            minValue = arr.Concat(new[] { minValue }).Min();
+
+            var dif = maxValue - minValue;
+
+            return dif;
+        }
+
+        private static void GetModifyArray(int[] arr)
+        {
+            var maxValue = arr[0];
+            var minValue = arr[0];
+
+            maxValue = arr.Concat(new[] { maxValue }).Max();
+            minValue = arr.Concat(new[] { minValue }).Min();
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    arr[i] += maxValue;
+                }
+                else
+                {
+                    arr[i] -= minValue;
+                }
+            }
         }
     }
 }
